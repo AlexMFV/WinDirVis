0405658efd2fc0618bb33c1dd9a4a32a5a32b166
Alex Valente <alexmfv1@gmail.com>
2025-08-02 20:33:46 +0100

    Project save commit

---
+++ WinDirVis/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WinDirVis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dirList = new System.Windows.Forms.DataGridView();
+            this.colFolder = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dirList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dirList
+            // 
+            this.dirList.AllowUserToDeleteRows = false;
+            this.dirList.AllowUserToOrderColumns = true;
+            this.dirList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dirList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colFolder});
+            this.dirList.Location = new System.Drawing.Point(12, 12);
+            this.dirList.Name = "dirList";
+            this.dirList.ReadOnly = true;
+            this.dirList.Size = new System.Drawing.Size(537, 537);
+            this.dirList.TabIndex = 0;
+            // 
+            // colFolder
+            // 
+            this.colFolder.HeaderText = "Folder";
+            this.colFolder.Name = "colFolder";
+            this.colFolder.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.dirList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dirList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dirList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFolder;
+    }
+}
+
---
+++ WinDirVis/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinDirVis.Logs;
+
+namespace WinDirVis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Gets all the systems logical drives
+            string[] drives = Environment.GetLogicalDrives();
+
+            //foreach(string drive in drives)
+                LoadPath(drives[0], recusionLimit: 1000);
+        }
+
+        /// <summary>
+        /// Recursive method to load all paths to t
+        /// </summary>
+        /// <param name="path"></param>
+        public int LoadPath(string path, int recusionLimit = 0, int level = 5)
+        {
+            if (recusionLimit > 0 && level == recusionLimit)
+                return level;
+
+            try
+            {
+                string[] subpaths = Directory.GetDirectories(path);
+
+                if (subpaths != null)
+                {
+                    foreach (string subpath in subpaths)
+                    {
+                        dirList.Rows.Add(subpath);
+                        LoadPath(subpath, recusionLimit, level+1);
+                    }
+                }
+
+                return level;
+            }
+            catch (Exception ex)
+            {
+                Logging.LogMessage(ex.Message);
+                return 0;
+            }
+        }
+    }
+}
